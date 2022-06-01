b026e4ea2e52eca0a9c87f0ad0ee9b4866c8ea3e
Justin Duong <jduong@umich.edu>
2022-05-31 22:59:02 -0400

    Added header css.

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const menuHamburger = document.getElementById('menu-hamburger');
+  const menuClose = document.getElementById('menu-close');
+  const menuList = document.querySelector('nav ul');
 
+  menuHamburger.addEventListener('click', () => {
+    menuHamburger.classList.add('hide');
+    menuClose.classList.remove('hide');
+    menuList.classList.remove('hide');
+  });
+
+  menuClose.addEventListener('click', () => {
+    menuHamburger.classList.remove('hide');
+    menuClose.classList.add('hide');
+    menuList.classList.add('hide');
+  });
 });
